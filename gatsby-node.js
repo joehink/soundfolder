ab7eefac32abd42719d1473aba9c1139e12e9917
Joe Hinkley <joehhinkley@gmail.com>
2019-12-24 14:27:43 -0800

    create a page for each sound

--- gatsby-node.js
+++ gatsby-node.js
@@ -4,4 +4,45 @@
  * See: https://www.gatsbyjs.org/docs/node-apis/
  */
 
-// You can delete this file if you're not using it
+const path = require(`path`);
+
+exports.onCreateNode = ({ node, getNode, actions }) => {
+    const { createNodeField } = actions;
+    if (node.internal.type === `SoundsCsv`) {
+        const slug = `/${node.title.toLowerCase().replace(' ', '_')}_${node.id}`;
+        createNodeField({
+            node,
+            name: `slug`,
+            value: slug,
+        });
+    }
+}
+
+exports.createPages = async ({ graphql, actions }) => {
+    const { createPage } = actions
+
+    const result = await graphql(`
+        query {
+            allSoundsCsv {
+                nodes {
+                    title
+                    fields {
+                        slug
+                    }
+                }
+            }
+        }
+    `)
+    
+    result.data.allSoundsCsv.nodes.forEach(node => {
+        createPage({
+            path: node.fields.slug,
+            component: path.resolve(`./src/templates/sound.js`),
+            context: {
+                // Data passed to context is available
+                // in page queries as GraphQL variables.
+                slug: node.fields.slug,
+            },
+        })
+    })
+}
