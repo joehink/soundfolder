f32ac0bc08070ff59906342d3393eb0be94744e2
Joe Hinkley <joehhinkley@gmail.com>
2019-12-29 19:29:03 -0800

    add photo to faq page

--- src/pages/faq.js
+++ src/pages/faq.js
@@ -1,10 +1,13 @@
 import React from "react";
+import Img from "gatsby-image"
 
 import Container from "../components/container";
 import Footer from "../components/footer";
 import Nav from "../components/nav";
+import { graphql } from "gatsby";
 
-export default () => {
+export default ({ data }) => {
+    console.log(data);
     return (
         <>
             <Container>
@@ -35,16 +38,19 @@ export default () => {
                         deserunt mollit anim id est laborum.
                     </p>
                     <h2>Who runs this website?</h2>
-                    <p>
-                        Lorem ipsum dolor sit amet, consectetur adipiscing elit, 
-                        sed do eiusmod tempor incididunt ut labore et dolore magna 
-                        aliqua. Ut enim ad minim veniam, quis nostrud exercitation 
-                        ullamco laboris nisi ut aliquip ex ea commodo consequat. 
-                        Duis aute irure dolor in reprehenderit in voluptate velit 
-                        esse cillum dolore eu fugiat nulla pariatur. Excepteur sint 
-                        occaecat cupidatat non proident, sunt in culpa qui officia 
-                        deserunt mollit anim id est laborum.
-                    </p>
+                    <div className="whoSection">
+                        <Img fluid={data.file.childImageSharp.fluid} className="joe" />
+                        <p>
+                            Lorem ipsum dolor sit amet, consectetur adipiscing elit, 
+                            sed do eiusmod tempor incididunt ut labore et dolore magna 
+                            aliqua. Ut enim ad minim veniam, quis nostrud exercitation 
+                            ullamco laboris nisi ut aliquip ex ea commodo consequat. 
+                            Duis aute irure dolor in reprehenderit in voluptate velit 
+                            esse cillum dolore eu fugiat nulla pariatur. Excepteur sint 
+                            occaecat cupidatat non proident, sunt in culpa qui officia 
+                            deserunt mollit anim id est laborum.
+                        </p>
+                    </div>
                     <h2>How can I contact Joe?</h2>
                     <p>
                         Lorem ipsum dolor sit amet, consectetur adipiscing elit, 
@@ -62,3 +68,15 @@ export default () => {
         </>
     )
 }
+
+export const query = graphql`
+    query {
+        file(relativePath: { eq: "joe.jpg" }) {
+            childImageSharp {
+                fluid(maxWidth: 175) {
+                    ...GatsbyImageSharpFluid
+                }
+            }
+          }
+    }
+`
