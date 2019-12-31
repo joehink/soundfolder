bdeddec92d29c67953be3466640bea0e9ae91d46
Joe Hinkley <joehhinkley@gmail.com>
2019-12-30 21:50:02 -0800

    add page for no search results

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,12 +1,21 @@
-import React from "react"
+import React, { useRef } from "react"
+import { FaChevronDown } from "react-icons/fa";
 
-import Hero from "../components/hero";
+import bomb from "../images/bomb_1.svg";
+
 import Layout from "../components/layout"
 import SEO from "../components/seo"
 import Container from "../components/container";
 import SoundCard from "../components/soundCard";
+import Nav from "../components/nav";
+import SearchBar from "../components/searchBar";
+
+const scrollToRef = (ref) => window.scrollTo(0, ref.current.offsetTop)   
 
 export default ({ data }) => {
+
+    const myRef = useRef(null)
+    const executeScroll = () => scrollToRef(myRef)
 
     const soundCards = data.allSoundsCsv.nodes.map(sound => {
         return <SoundCard sound={sound} key={sound.id} />
@@ -16,9 +25,19 @@ export default ({ data }) => {
         <Layout>
             <SEO title="Home" />
             <div id="index">
-                <Hero />
+                <header className="hero">
+                    <Container>
+                        <Nav white />
+                        <img src={bomb} alt="placeholder" className="heroImg" />
+                        <SearchBar />
+                        <h2>Totally free sound effects. No attribution required.</h2>
+                        <button className="recent" onClick={executeScroll}>
+                            <i><FaChevronDown /></i>Recently added
+                        </button>
+                    </Container>
+                </header>
                 <Container>
-                    <h2 className="recentHeading">Recently added</h2>
+                    <h2 ref={myRef} className="recentHeading">Recently added</h2>
                     <div className="sounds">
                         { soundCards }
                     </div>
--- src/pages/search.js
+++ src/pages/search.js
@@ -1,4 +1,6 @@
-import React, { useState, useEffect } from 'react';
+import React, { Component } from 'react';
+
+import muteIcon from "../images/mute.svg";
 
 import Container from "../components/container"; 
 import Layout from "../components/layout";
@@ -6,35 +8,63 @@ import Nav from "../components/nav";
 import SEO from "../components/seo";
 import SoundCard from "../components/soundCard";
 
-export default ({ location }) => {
-    const [results, setResults] = useState([]);
-    const searchQuery = new URLSearchParams(location.search).get('keywords') || '';
 
+export default class Search extends Component {
+    constructor(props) {
+        super(props);
 
-    useEffect(() => {
+        this.state = {
+            results: [],
+            searchQuery: ''
+        }
+
+        this.renderSounds = this.renderSounds.bind(this);
+    }
+    componentDidMount() {
+        const searchQuery = new URLSearchParams(this.props.location.search).get('keywords') || '';
+
         if (window.__LUNR__) {
             window.__LUNR__.__loaded.then(lunr => {
                 const refs = lunr.en.index.search(searchQuery);
                 const sounds = refs.map(({ ref }) => lunr.en.store[ref]);
-                setResults(sounds);
+                this.setState({
+                    results: sounds,
+                    searchQuery
+                })
             });
         }
-    }, []);
-    
-    const soundCards = results.map(sound => <SoundCard sound={sound} key={sound.id} />)
-    return (
-        <Layout>
-            <SEO title="Search" />
-            <div id="search">
-                <Container>
-                    <Nav />
-                    <h2>{results.length} search result{ results.length === 1 ? '' : 's' } for <span className="searchTerm">"{searchQuery}"</span></h2>
-                    <div className="sounds">
-                        { soundCards }
-                    </div>
-                </Container>
-            </div>
-            
-        </Layout>
-    )
+    }
+    renderSounds() {
+        if (!this.state.searchQuery || !this.state.results.length) {
+            return (
+                <div className="noResults">
+                    <img src={muteIcon} alt="No results" />
+                    <h2>0 search results for <span className="searchTerm">"{this.state.searchQuery}"</span></h2>
+                </div>
+            )
+        }
+        const soundCards = this.state.results.map(sound => <SoundCard sound={sound} key={sound.id} />)
+
+        return (
+            <>
+                <h2>{this.state.results.length} search result{ this.state.results.length === 1 ? '' : 's' } for <span className="searchTerm">"{this.state.searchQuery}"</span></h2>
+                <div className="sounds">
+                    { soundCards }
+                </div>
+            </>
+        )
+    }
+    render() {
+        return (
+            <Layout>
+                <SEO title="Search" />
+                <div id="search">
+                    <Container>
+                        <Nav />
+                        { this.renderSounds() }
+                    </Container>
+                </div>
+            </Layout>
+        )
+    }
 }
